136b9777b49be0e70f0e3c82662595285421a306
Rich Wareham <rjw57@cam.ac.uk>
2015-03-10 15:17:37 +0000

    manager: add mktoken command to generate tokens for users

--- cubbie/manager.py
+++ cubbie/manager.py
@@ -16,6 +16,7 @@ from cubbie.fixture import (
     create_user_fixtures, create_production_fixtures,
     create_performance_fixtures, create_sales_fixtures, create_capability_fixtures
 )
+from cubbie.auth import make_user_token
 
 def create_manager_app(config=None):
     app = create_app()
@@ -25,28 +26,36 @@ def create_manager_app(config=None):
 
     return app
 
-class GenFakeData(Command):
-    "generates fake data in the database"
-
-    def run(self):
-        if not current_app.debug:
-            logging.error(
-                'genfake command is only available in deebug modfe. '
-                'Ensure that DEBUG is True in app config.'
-            )
-            return
-
-        db.create_all()
-        mixer.init_app(current_app)
-        create_user_fixtures(10)
-        create_production_fixtures(5)
-        create_capability_fixtures(10)
-        create_performance_fixtures(25)
-        create_sales_fixtures(200)
-
 manager = Manager(create_manager_app)
+
+@manager.command
+def genfake():
+    if not current_app.debug:
+        logging.error(
+            'genfake command is only available in deebug modfe. '
+            'Ensure that DEBUG is True in app config.'
+        )
+        return
+
+    db.create_all()
+    mixer.init_app(current_app)
+    create_user_fixtures(10)
+    create_production_fixtures(5)
+    create_capability_fixtures(10)
+    create_performance_fixtures(25)
+    create_sales_fixtures(200)
+
+@manager.command
+def mktoken(userid):
+    u = User.query.get(userid)
+    if u is None:
+        logging.error('No such user: %s' % userid)
+        return
+
+    token = make_user_token(u)
+    print(token)
+
 manager.add_command('db', MigrateCommand)
-manager.add_command('genfake', GenFakeData)
 manager.add_option('-c', '--config', dest='config', required=False)
 
 def main():
