32e13e5987837714b79073a3ff186dfe87959531
Rich Wareham <rjw57@cam.ac.uk>
2015-03-10 14:58:16 +0000

    add list of user's productions to profile response

--- cubbie/tests/test_user_api.py
+++ cubbie/tests/test_user_api.py
@@ -5,8 +5,10 @@ Test user profile API.
 
 from flask import url_for
 
+from cubbie.model import Capability, Production, User
+
 def auth_headers(token):
-    """Return a list of Authorization headers correponding to token."""
+    """Return a list of Authorization headers corresponding to token."""
     return [('Authorization', 'Bearer %s' % token)]
 
 def test_profile_needs_auth(client):
@@ -20,3 +22,22 @@ def test_profile(client, member_token, member_user):
     assert resp.status_code == 200
     assert resp.json['_type'] == 'User#profile'
     assert resp.json['displayname'] == member_user.displayname
+
+def test_profile_productions(client, member_token, member_user, mixer):
+    """The user is a member of all the productions in the profile."""
+    # Add user as a member to some more productions
+    mixer.cycle(3).blend(Capability, user=member_user, production=mixer.SELECT, type='member')
+
+    # Getting profile succeeds
+    resp = client.get(url_for('api.profile'), headers=auth_headers(member_token))
+    assert resp.status_code == 200
+
+    # The number of productions match
+    resp_prods = resp.json['productions']
+    assert len(resp_prods) == len(member_user.productions)
+
+    # The user is indeed a member (of some sort) of each production
+    for p_record in resp_prods:
+        p = Production.query.get(p_record['id'])
+        assert member_user in p.users
+        assert p in member_user.productions
--- cubbie/webapp.py
+++ cubbie/webapp.py
@@ -5,7 +5,7 @@ WSGI-compatible web application object for cubbie.
 from flask import Flask, Blueprint, jsonify
 from flask_jwt import jwt_required, JWT, current_user
 
-from cubbie.model import db, User
+from cubbie.model import db, User, Production, Capability
 
 jwt = JWT()
 
@@ -21,9 +21,18 @@ api = Blueprint('api', __name__)
 @api.route('/profile')
 @jwt_required()
 def profile():
+    # Find productions user is a part of
+    productions_q = Production.query.join(Capability).\
+        filter(Capability.user == current_user)
+    productions = list(
+        dict(id=p.id)
+        for p in productions_q
+    )
+
     return jsonify({
         '_type': 'User#profile',
         'displayname': current_user.displayname,
+        'productions': productions,
     })
 
 @jwt.user_handler
